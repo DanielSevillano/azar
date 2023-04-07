f565d71410672c1f5d99487ff710935fa08322a6
Daniel <daniel-sevillano@hotmail.com>
2023-04-07 19:11:09 +0200

    Ajustes menores.

--- app/src/main/java/com/daniel/azar/Historial.kt
+++ app/src/main/java/com/daniel/azar/Historial.kt
@@ -1,5 +1,6 @@
 package com.daniel.azar
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -20,7 +21,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun Historial(
     cerrarHistorial: () -> Unit,
@@ -39,37 +40,39 @@ fun Historial(
             contentPadding = PaddingValues(start = 16.dp, end = 16.dp, bottom = 20.dp),
             verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
-            item {
-                Column {
-                    Row(
-                        horizontalArrangement = Arrangement.spacedBy(8.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Icon(
-                            imageVector = Icons.Outlined.History,
-                            contentDescription = stringResource(id = R.string.historial)
-                        )
-
-                        Text(
-                            text = stringResource(id = R.string.historial),
-                            style = MaterialTheme.typography.titleLarge
-                        )
-
-                        Spacer(modifier = Modifier.weight(1f))
-
-                        IconButton(onClick = { eliminarHistorial() }) {
+            stickyHeader {
+                Surface {
+                    Column {
+                        Row(
+                            horizontalArrangement = Arrangement.spacedBy(8.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
                             Icon(
-                                imageVector = Icons.Outlined.Delete,
-                                contentDescription = stringResource(id = R.string.historial_eliminar)
+                                imageVector = Icons.Outlined.History,
+                                contentDescription = stringResource(id = R.string.historial)
                             )
-                        }
-                    }
 
-                    Divider(
-                        modifier = Modifier
-                            .padding(top = 8.dp, bottom = 12.dp)
-                            .alpha(0.2F)
-                    )
+                            Text(
+                                text = stringResource(id = R.string.historial),
+                                style = MaterialTheme.typography.titleLarge
+                            )
+
+                            Spacer(modifier = Modifier.weight(1f))
+
+                            FilledTonalIconButton(onClick = { eliminarHistorial() }) {
+                                Icon(
+                                    imageVector = Icons.Outlined.Delete,
+                                    contentDescription = stringResource(id = R.string.historial_eliminar)
+                                )
+                            }
+                        }
+
+                        Divider(
+                            modifier = Modifier
+                                .padding(top = 6.dp)
+                                .alpha(0.2F)
+                        )
+                    }
                 }
             }
 
@@ -77,15 +80,20 @@ fun Historial(
                 item {
                     Text(
                         text = stringResource(id = R.string.historial_vacio),
-                        modifier = Modifier.fillMaxWidth(),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 8.dp),
                         textAlign = TextAlign.Center
                     )
                 }
             }
 
             items(tiradasElemento.reversed()) { tirada ->
-                Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-                    tirada.valoresObtenidos.forEach {  valor ->
+                Row(
+                    modifier = Modifier.padding(top = 8.dp),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    tirada.valoresObtenidos.forEach { valor ->
                         when (elementoSeleccionado) {
                             Elemento.Rango -> {
                                 Text(
